ee105d433cbd932f6d5d5593d20ef8db89d7f68e
v9lu <gupalo228@gmail.com>
2022-10-18 00:29:21 +0200

    Updated: db.py (1.1.0 -> 2.0.0) -Migrated from sqlite to mysql database

--- sp/db.py
+++ sp/db.py
@@ -1,22 +1,40 @@
-# Version 1.1.0 release
+# Version 2.0.0 release
+import mysql.connector
 
-def initialization(conn):
+
+def initialization():
+    # Create server_db if not exists
+    conn = mysql.connector.connect(user="root", password="174537",
+                                   host="188.166.104.222")
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS neewsfeeds_old_ids (id INT PRIMARY KEY)''')
+    cursor.execute("CREATE DATABASE IF NOT EXISTS newsfeeds_db")
+
+    # Create tables if not exists
+    conn = mysql.connector.connect(user="root", password="174537",
+                                   host="188.166.104.222", database="newsfeeds_db")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS old_ids "
+                   "(user_id INT, newsfeed_id INT)")
+
+    # Save changes
     conn.commit()
     cursor.close()
 
 
-def newsfeeds_is_old_id(conn, newsfeed_id):
+def newsfeeds_is_old_id(user_id: int, newsfeed_id: int):
+    conn = mysql.connector.connect(user="root", password="174537",
+                                   host="188.166.104.222", database="newsfeeds_db")
     cursor = conn.cursor()
-    response = cursor.execute(f"SELECT EXISTS (SELECT 1 FROM neewsfeeds_old_ids WHERE id=?)", (newsfeed_id,))
-    id_exists = response.fetchone()[0]
+    cursor.execute("SELECT EXISTS (SELECT 1 FROM old_ids WHERE user_id=%s AND newsfeed_id=%s)", (user_id, newsfeed_id))
+    id_exists = cursor.fetchone()[0]
     cursor.close()
     return bool(id_exists)
 
 
-def newsfeeds_add_old_id(conn, newsfeed_id):
+def newsfeeds_add_old_id(user_id: int, newsfeed_id: int):
+    conn = mysql.connector.connect(user="root", password="174537",
+                                   host="188.166.104.222", database="newsfeeds_db")
     cursor = conn.cursor()
-    cursor.execute("INSERT OR IGNORE INTO neewsfeeds_old_ids (id) VALUES (?)", (newsfeed_id,))
+    cursor.execute("INSERT IGNORE INTO old_ids (user_id, newsfeed_id) VALUES (%s, %s)", (user_id, newsfeed_id))
     conn.commit()
     cursor.close()
