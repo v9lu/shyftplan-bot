4318d1c76cecb8494680c6fa627995ac6e81625c
v9lu <gupalo228@gmail.com>
2022-10-25 03:24:07 +0200

    Updated: telegram_data.py -> config_data.py (1.0.0 -> 1.1.0)

---
+++ sp/config_data.py
@@ -0,0 +1,23 @@
+# Version 1.1.0 release
+
+from configparser import ConfigParser
+
+
+def get_bot_token(config: ConfigParser) -> str:
+    config.read('settings.ini')
+    return config.get("TELEGRAM", "bot_token")
+
+
+def get_db(config: ConfigParser) -> dict:
+    config.read('settings.ini')
+    return {"ip": config.get("DATABASE", "ip"),
+            "password": config.get("DATABASE", "password")}
+
+
+def get_user(config: ConfigParser) -> dict:
+    config.read('settings.ini')
+    return {"shyftplan_email": config.get("AUTH_CONFIG", "shyftplan_email"),
+            "shyftplan_token": config.get("AUTH_CONFIG", "shyftplan_token"),
+            "shyftplan_employee_id": config.getint("AUTH_CONFIG", "shyftplan_employee_id"),
+            "shyftplan_user_id": config.getint("AUTH_CONFIG", "shyftplan_user_id"),
+            "telegram_id": config.getint("AUTH_CONFIG", "telegram_id")}
