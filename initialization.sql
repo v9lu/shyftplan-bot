5a48b59f4e453f4799dc5dcd771448290142f1bf
v9lu <gupalo228@gmail.com>
2022-10-25 03:48:12 +0200

    -Created initialization sql script

---
+++ initialization.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS newsfeeds_db;
+CREATE DATABASE IF NOT EXISTS keys_db;
+
+USE newsfeeds_db;
+
+CREATE TABLE IF NOT EXISTS old_ids (
+    user_id INT,
+    newsfeed_id INT
+);
+
+USE keys_db;
+
+CREATE TABLE IF NOT EXISTS activation_keys (
+    activation_key TINYTEXT,
+    key_type TINYTEXT,
+    key_expire DATETIME
+);
