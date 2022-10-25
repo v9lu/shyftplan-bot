ebd93ec5dd4997cad898bcf27550b2b5a51536a9
v9lu <gupalo228@gmail.com>
2022-10-25 03:30:25 +0200

    Removed useless files

--- sp/telegram_data.py
+++
@@ -1,9 +0,0 @@
-# Version 1.0.0 release
-
-from configparser import ConfigParser
-
-
-def get(config: ConfigParser) -> dict:
-    config.read('settings.ini')
-    return {"account_id": config.getint("TELEGRAM", "account_id"),
-            "bot_token": config.get("TELEGRAM", "bot_token")}
