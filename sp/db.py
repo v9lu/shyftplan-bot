7275ee1db12d4a9d46750451e5b1fbc316f9e0cc
v9lu <gupalo228@gmail.com>
2022-10-25 03:26:22 +0200

    Updated: db.py (2.0.0 -> 2.1.0) -Removed conn initialization from all functions -Removed initialization function

--- sp/db.py
+++ sp/db.py
@@ -1,29 +1,9 @@
-# Version 2.0.0 release
-import mysql.connector
+# Version 2.1.0 release
+
+from mysql.connector.connection_cext import CMySQLConnection
 
 
-def initialization():
-    # Create server_db if not exists
-    conn = mysql.connector.connect(user="root", password="174537",
-                                   host="188.166.104.222")
-    cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS newsfeeds_db")
-
-    # Create tables if not exists
-    conn = mysql.connector.connect(user="root", password="174537",
-                                   host="188.166.104.222", database="newsfeeds_db")
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS old_ids "
-                   "(user_id INT, newsfeed_id INT)")
-
-    # Save changes
-    conn.commit()
-    cursor.close()
-
-
-def newsfeeds_is_old_id(user_id: int, newsfeed_id: int):
-    conn = mysql.connector.connect(user="root", password="174537",
-                                   host="188.166.104.222", database="newsfeeds_db")
+def newsfeeds_is_old_id(conn: CMySQLConnection, user_id: int, newsfeed_id: int):
     cursor = conn.cursor()
     cursor.execute("SELECT EXISTS (SELECT 1 FROM old_ids WHERE user_id=%s AND newsfeed_id=%s)", (user_id, newsfeed_id))
     id_exists = cursor.fetchone()[0]
@@ -31,9 +11,7 @@ def newsfeeds_is_old_id(user_id: int, newsfeed_id: int):
     return bool(id_exists)
 
 
-def newsfeeds_add_old_id(user_id: int, newsfeed_id: int):
-    conn = mysql.connector.connect(user="root", password="174537",
-                                   host="188.166.104.222", database="newsfeeds_db")
+def newsfeeds_add_old_id(conn: CMySQLConnection, user_id: int, newsfeed_id: int):
     cursor = conn.cursor()
     cursor.execute("INSERT IGNORE INTO old_ids (user_id, newsfeed_id) VALUES (%s, %s)", (user_id, newsfeed_id))
     conn.commit()
