f7a8becebd4c1dede4cfa2084026f8bee707dd19
v9lu <gupalo228@gmail.com>
2022-11-07 17:54:52 +0100

    Updated: initialization.sql -Created new database (users_db) -Created new tables (users_configs, users_statistics, users_subscriptions)

--- initialization.sql
+++ initialization.sql
@@ -1,5 +1,14 @@
-CREATE DATABASE IF NOT EXISTS newsfeeds_db;
 CREATE DATABASE IF NOT EXISTS keys_db;
+CREATE DATABASE IF NOT EXISTS newsfeeds_db;
+CREATE DATABASE IF NOT EXISTS users_db;
+
+USE keys_db;
+
+CREATE TABLE IF NOT EXISTS activation_keys (
+    activation_key TINYTEXT,
+    key_type TINYTEXT,
+    key_days INT
+);
 
 USE newsfeeds_db;
 
@@ -8,10 +17,32 @@ CREATE TABLE IF NOT EXISTS old_ids (
     newsfeed_id INT
 );
 
-USE keys_db;
+USE users_db;
 
-CREATE TABLE IF NOT EXISTS activation_keys (
-    activation_key TINYTEXT,
-    key_type TINYTEXT,
-    key_days INT
+CREATE TABLE IF NOT EXISTS users_configs (
+    user_id INT NOT NULL PRIMARY KEY,
+    sp_email TINYTEXT DEFAULT NULL,
+    sp_pass TINYTEXT DEFAULT NULL,
+    sp_eid INT DEFAULT NULL,
+    sp_uid INT DEFAULT NULL,
+    prog_status BOOL NOT NULL DEFAULT 1,
+    prog_open_shifts BOOL NOT NULL DEFAULT 1,
+    prog_shift_offers BOOL NOT NULL DEFAULT 1,
+    prog_news BOOL NOT NULL DEFAULT 0,
+    prog_sleep FLOAT(3, 2) NOT NULL DEFAULT 1,
+    shifts MEDIUMTEXT DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_statistics (
+    user_id INT NOT NULL PRIMARY KEY,
+    shifted_shifts INT DEFAULT 0,
+    shifted_hours FLOAT(8, 2) DEFAULT 0,
+    earned FLOAT(9, 2) DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS users_subscriptions (
+    user_id INT NOT NULL PRIMARY KEY,
+    subscription TINYTEXT DEFAULT NULL,
+    expire DATETIME DEFAULT NULL,
+    used_trial BOOL DEFAULT 0
 );
