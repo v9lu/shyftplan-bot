8b29cc3019e15c74880db49d5fbe09d208882c63
v9lu <gupalo228@gmail.com>
2022-11-09 09:59:11 +0100

    Updated: initialization.sql -Changed default "prog_sleep" from 1 to 5 -Created new database "sp_users_db" -Renamed "sp_pass" to "sp_token"

--- initialization.sql
+++ initialization.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS keys_db;
 CREATE DATABASE IF NOT EXISTS newsfeeds_db;
+CREATE DATABASE IF NOT EXISTS sp_users_db;
 CREATE DATABASE IF NOT EXISTS users_db;
 
 USE keys_db;
@@ -17,19 +18,28 @@ CREATE TABLE IF NOT EXISTS old_ids (
     newsfeed_id INT
 );
 
+USE sp_users_db;
+
+CREATE TABLE IF NOT EXISTS sp_users_subscriptions (
+    sp_uid INT NOT NULL PRIMARY KEY,
+    subscription TINYTEXT DEFAULT NULL,
+    expire DATETIME DEFAULT NULL,
+    used_trial BOOL DEFAULT 0
+);
+
 USE users_db;
 
 CREATE TABLE IF NOT EXISTS users_configs (
     user_id INT NOT NULL PRIMARY KEY,
     sp_email TINYTEXT DEFAULT NULL,
-    sp_pass TINYTEXT DEFAULT NULL,
+    sp_token TINYTEXT DEFAULT NULL,
     sp_eid INT DEFAULT NULL,
     sp_uid INT DEFAULT NULL,
     prog_status BOOL NOT NULL DEFAULT 1,
     prog_open_shifts BOOL NOT NULL DEFAULT 1,
     prog_shift_offers BOOL NOT NULL DEFAULT 1,
     prog_news BOOL NOT NULL DEFAULT 0,
-    prog_sleep FLOAT(3, 2) NOT NULL DEFAULT 1,
+    prog_sleep FLOAT(3, 2) NOT NULL DEFAULT 5,
     shifts MEDIUMTEXT DEFAULT NULL
 );
 
@@ -38,11 +48,4 @@ CREATE TABLE IF NOT EXISTS users_statistics (
     shifted_shifts INT DEFAULT 0,
     shifted_hours FLOAT(8, 2) DEFAULT 0,
     earned FLOAT(9, 2) DEFAULT 0
-);
-
-CREATE TABLE IF NOT EXISTS users_subscriptions (
-    user_id INT NOT NULL PRIMARY KEY,
-    subscription TINYTEXT DEFAULT NULL,
-    expire DATETIME DEFAULT NULL,
-    used_trial BOOL DEFAULT 0
 );
