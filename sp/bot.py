98bbbd7a29d5b58c7d665bd9d60429172a7f0ee4
v9lu <gupalo228@gmail.com>
2022-11-09 10:13:39 +0100

    Updated: bot.py (2.0.2 -> 2.0.3) -Adapted for updated "config_data" -Added new "bot_keys" import

--- sp/bot.py
+++ sp/bot.py
@@ -1,4 +1,4 @@
-# Version 2.0.2 release
+# Version 2.0.3 release
 
 import asyncio
 import configparser
@@ -8,17 +8,19 @@ from aiogram import Bot, Dispatcher
 import bot_common
 import bot_authorization
 import bot_update_shifts
+import bot_keys
 import config_data
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    bot = Bot(token=config_data.get_bot_token(configparser.ConfigParser()))
+    bot = Bot(token=config_data.get_bot(configparser.ConfigParser())["bot_token"])
     dp = Dispatcher()
 
     dp.include_router(bot_common.router)
     dp.include_router(bot_authorization.router)
     dp.include_router(bot_update_shifts.router)
+    dp.include_router(bot_keys.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
