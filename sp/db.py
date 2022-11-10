a1c2ea955ce7652532a7cddaf385d4ece1ee3601
v9lu <gupalo228@gmail.com>
2022-11-10 09:45:07 +0100

    Updated: db.py (2.2.0 -> 2.2.1) -Renamed some functions args

--- sp/db.py
+++ sp/db.py
@@ -1,4 +1,4 @@
-# Version 2.2.0 release
+# Version 2.2.1 release
 
 from mysql.connector.connection_cext import CMySQLConnection
 
@@ -31,17 +31,17 @@ def keys_remove_key(conn: CMySQLConnection, key: str):
     cursor.close()
 
 
-def newsfeeds_is_old_id(conn: CMySQLConnection, user_id: int, newsfeed_id: int) -> bool:
+def newsfeeds_is_old_id(conn: CMySQLConnection, sp_uid: int, newsfeed_id: int) -> bool:
     cursor = conn.cursor()
-    cursor.execute("SELECT EXISTS (SELECT 1 FROM old_ids WHERE user_id=%s AND newsfeed_id=%s)", (user_id, newsfeed_id))
+    cursor.execute("SELECT EXISTS (SELECT 1 FROM old_ids WHERE user_id=%s AND newsfeed_id=%s)", (sp_uid, newsfeed_id))
     id_exists = cursor.fetchone()[0]
     cursor.close()
     return bool(id_exists)
 
 
-def newsfeeds_add_old_id(conn: CMySQLConnection, user_id: int, newsfeed_id: int):
+def newsfeeds_add_old_id(conn: CMySQLConnection, sp_uid: int, newsfeed_id: int):
     cursor = conn.cursor()
-    cursor.execute("INSERT IGNORE INTO old_ids (user_id, newsfeed_id) VALUES (%s, %s)", (user_id, newsfeed_id))
+    cursor.execute("INSERT IGNORE INTO old_ids (user_id, newsfeed_id) VALUES (%s, %s)", (sp_uid, newsfeed_id))
     conn.commit()
     cursor.close()
 
