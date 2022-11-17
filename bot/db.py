5f011fb1c77870b119842d177cdde41da687529f
v9lu <gupalo228@gmail.com>
2022-11-17 21:45:27 +0100

    Updated: db.py (2.2.4 -> 2.3.0) -Adapted for new mysql db structure

--- bot/db.py
+++ bot/db.py
@@ -1,8 +1,9 @@
-# Version 2.2.4 release
+# Version 2.3.0 release
 
 from mysql.connector.connection_cext import CMySQLConnection
 
 
+# KEYS_DB
 def keys_activate_key(conn: CMySQLConnection, key: str):
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM activation_keys WHERE activation_key=%s", (key,))
@@ -32,6 +33,7 @@ def keys_remove_key(conn: CMySQLConnection, key: str):
     cursor.close()
 
 
+# NEWSFEEDS_DB
 def newsfeeds_is_old_id(conn: CMySQLConnection, sp_uid: int, newsfeed_id: int) -> bool:
     cursor = conn.cursor()
     cursor.execute("SELECT EXISTS (SELECT 1 FROM old_ids WHERE user_id=%s AND newsfeed_id=%s)", (sp_uid, newsfeed_id))
@@ -47,22 +49,35 @@ def newsfeeds_add_old_id(conn: CMySQLConnection, sp_uid: int, newsfeed_id: int):
     cursor.close()
 
 
+# SP_USERS_DB
 def sp_users_add_user(conn: CMySQLConnection, sp_uid: int) -> None:
     cursor = conn.cursor()
+    cursor.execute("INSERT IGNORE INTO sp_users_configs (sp_uid) VALUES (%s)", (sp_uid,))
     cursor.execute("INSERT IGNORE INTO sp_users_subscriptions (sp_uid) VALUES (%s)", (sp_uid,))
     conn.commit()
     cursor.close()
 
 
-def sp_users_sub_info(conn: CMySQLConnection, sp_uid: int) -> dict:
+def sp_users_get_user(conn: CMySQLConnection, sp_uid: int) -> dict:
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM sp_users_subscriptions WHERE sp_uid=%s", (sp_uid,))
+    cursor.execute("SELECT * FROM sp_users_configs, sp_users_subscriptions "
+                   "WHERE sp_users_configs.sp_uid=%s AND sp_users_subscriptions.sp_uid=%s", (sp_uid, sp_uid))
     user_data = cursor.fetchone()
     cursor.close()
     return user_data
 
 
-def sp_users_update_user(conn: CMySQLConnection, sp_uid: int, **kwargs) -> None:
+def sp_users_configs_update_user(conn: CMySQLConnection, sp_uid: int, **kwargs) -> None:
+    cursor = conn.cursor()
+    for arg in kwargs.items():
+        arg_name = arg[0]
+        arg_data = arg[1]
+        cursor.execute(f"UPDATE sp_users_configs SET {arg_name}=%s WHERE sp_uid=%s", (arg_data, sp_uid))
+    conn.commit()
+    cursor.close()
+
+
+def sp_users_subscriptions_update_user(conn: CMySQLConnection, sp_uid: int, **kwargs) -> None:
     cursor = conn.cursor()
     for arg in kwargs.items():
         arg_name = arg[0]
@@ -72,9 +87,10 @@ def sp_users_update_user(conn: CMySQLConnection, sp_uid: int, **kwargs) -> None:
     cursor.close()
 
 
+# USERS_DB
 def users_add_user(conn: CMySQLConnection, user_id: int) -> None:
     cursor = conn.cursor()
-    cursor.execute("INSERT IGNORE INTO users_configs (user_id) VALUES (%s)", (user_id,))
+    cursor.execute("INSERT IGNORE INTO users_auth (user_id) VALUES (%s)", (user_id,))
     cursor.execute("INSERT IGNORE INTO users_statistics (user_id) VALUES (%s)", (user_id,))
     conn.commit()
     cursor.close()
@@ -82,19 +98,19 @@ def users_add_user(conn: CMySQLConnection, user_id: int) -> None:
 
 def users_get_user(conn: CMySQLConnection, user_id: int) -> dict:
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users_configs, users_statistics "
-                   "WHERE users_configs.user_id=%s AND users_statistics.user_id=%s", (user_id, user_id))
+    cursor.execute("SELECT * FROM users_auth, users_statistics "
+                   "WHERE users_auth.user_id=%s AND users_statistics.user_id=%s", (user_id, user_id))
     user_data = cursor.fetchone()
     cursor.close()
     return user_data
 
 
-def users_configs_update_user(conn: CMySQLConnection, user_id: int, **kwargs) -> None:
+def users_auth_update_user(conn: CMySQLConnection, user_id: int, **kwargs) -> None:
     cursor = conn.cursor()
     for arg in kwargs.items():
         arg_name = arg[0]
         arg_data = arg[1]
-        cursor.execute(f"UPDATE users_configs SET {arg_name}=%s WHERE user_id=%s", (arg_data, user_id))
+        cursor.execute(f"UPDATE users_auth SET {arg_name}=%s WHERE user_id=%s", (arg_data, user_id))
     conn.commit()
     cursor.close()
 
