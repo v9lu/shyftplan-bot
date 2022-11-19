2d04b648a8fc6339f77411d54c0c98d200422a20
v9lu <gupalo228@gmail.com>
2022-11-19 17:58:15 +0100

    Updated: config_data.py (1.2.0 -> 1.2.1) -Changed directory name for new file architecture

--- tools/config_data.py
+++ tools/config_data.py
@@ -1,16 +1,16 @@
-# Version 1.2.0 release
+# Version 1.2.1 release
 
 from configparser import ConfigParser
 
 
 def get_bot(config: ConfigParser) -> dict:
-    config.read('settings.ini')
+    config.read('tools/settings.ini')
     return {"bot_token": config.get("TELEGRAM", "bot_token"),
             "pay_token": config.get("TELEGRAM", "pay_token")}
 
 
 def get_db(config: ConfigParser) -> dict:
-    config.read('settings.ini')
+    config.read('tools/settings.ini')
     return {"ip": config.get("DATABASE", "ip"),
             "port": config.getint("DATABASE", "port"),
             "password": config.get("DATABASE", "password")}
