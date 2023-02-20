182e3677cf3277f2f8110bd9e1036b768227a151
v9lu <gupalo228@gmail.com>
2023-02-20 21:47:19 +0100

    Updated: initialization.sql -Removed newsfeeds_db

--- initialization.sql
+++ initialization.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE IF NOT EXISTS keys_db;
-CREATE DATABASE IF NOT EXISTS newsfeeds_db;
 CREATE DATABASE IF NOT EXISTS sp_users_db;
 CREATE DATABASE IF NOT EXISTS users_db;
 
@@ -9,13 +8,6 @@ CREATE TABLE IF NOT EXISTS activation_keys (
     activation_key TINYTEXT,
     key_type TINYTEXT,
     key_days INT
-);
-
-USE newsfeeds_db;
-
-CREATE TABLE IF NOT EXISTS old_ids (
-    user_id INT,
-    newsfeed_id INT
 );
 
 USE sp_users_db;
