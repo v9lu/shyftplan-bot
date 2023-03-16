f1505fe757b505c17aec70c9edaa8c7b9a1a6745
v9lu <gupalo228@gmail.com>
2023-03-16 23:04:54 +0100

    Updated: config_data.py (1.4.0 -> 1.4.1) -Added get_user_for_exploit function for get user data with rights for exploit execution

--- tools/config_data.py
+++ tools/config_data.py
@@ -1,4 +1,4 @@
-# Version 1.3.0 release
+# Version 1.4.0 release
 
 from configparser import ConfigParser
 
@@ -21,3 +21,9 @@ def get_subs(config: ConfigParser) -> dict:
     return {"trial": config.getint("SUBSCRIPTION", "trial"),
             "standard": config.getint("SUBSCRIPTION", "standard"),
             "premium": config.getint("SUBSCRIPTION", "premium")}
+
+
+def get_user_for_exploit(config: ConfigParser) -> dict:
+    config.read('tools/settings.ini')
+    return {"user_email": config.get("USER_FOR_EXPLOIT", "user_email"),
+            "user_token": config.get("USER_FOR_EXPLOIT", "user_token")}
